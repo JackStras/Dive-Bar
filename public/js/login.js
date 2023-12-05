ecdcfb8aeb922a2e9ba5ba152e23d6646de45d1c
eli-soto <142184979+eli-soto@users.noreply.github.com>
2023-12-04 19:30:52 -0500

    login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,8 +1,8 @@
 let loginFormHandler = async (event) => {
     event.preventDefault();
   
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+    let email = document.querySelector('#email-login').value.trim();
+    let password = document.querySelector('#password-login').value.trim();
   
     if (email && password) {
       const response = await fetch('/api/users/login', {
@@ -14,7 +14,7 @@ let loginFormHandler = async (event) => {
       if (response.ok) {
         document.location.replace('/');
       } else {
-        alert('Failed to log in.');
+        alert('You did not login');
       }
     }
   };
