fa6b3f6ea5959ef522b3e2f1af4733e4e77d1a7e
cstasney <cstasney07@gmail.com>
2023-12-04 20:58:11 -0500

    get for homepage, updated matches.handlebar filename

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,41 @@
 const router = require('express').Router();
-const { Project, User } = require('../models');
+const { User } = require('../models');
 const withAuth = require('../utils/auth');
+
+// redner homepage if signed in
+router.get('/', withAuth, async (req, res) => {
+    try {
+        res.render('homepage', {
+            logged_in: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/', withAuth, async (req, res) => {
+  try {
+    // Get all projects and JOIN with user data
+    const userData = await User.findByPk({
+      include: [
+        {
+          model: User,
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const users = userData.map((matches) => matches.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      users, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // withAuth to prevent access to users profile page
 router.get('/profile', withAuth, async (req, res) => {
