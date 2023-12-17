d234f46374e2f6cd02b8b045f66bbb9a0af852d7
cstasney <cstasney07@gmail.com>
2023-12-17 17:27:48 -0500

    comment reply boxes

---
+++ public/js/comments.js
@@ -0,0 +1,8 @@
+const reply = document.getElementsByTagName('button')
+
+const replyHandler = (comments) => {
+}
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', replyHandler)
+}
